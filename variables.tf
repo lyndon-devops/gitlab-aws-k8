70645b3d3d42e6be14ea1a14f6d74f003f913bbd
LM Bibera <lyndon.bibera@headhuntr.io>
2021-09-03 14:38:20 +0800

    create vpc

--- variables.tf
+++ variables.tf
@@ -3,7 +3,12 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "cluster_name" {
+variable "application_name" {
   type    = string
-  default = "gitlab-eks"
+  default = "gitlab-demo"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
 }
---
+++ vpc.tf
@@ -0,0 +1,23 @@
+locals {
+  public_private_subnets = cidrsubnets(var.vpc_cidr, 4, 4)
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "${var.application_name}-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  private_subnets = cidrsubnets(local.public_private_subnets[0], 4, 4, 4)
+  public_subnets  = cidrsubnets(local.public_private_subnets[1], 4, 4, 4)
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+
+  tags = {
+    Application = var.application_name
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
